15a45c28f24083b5ede5cd6cbdafd6964370e417
Sarah Aldous <sealdous@gmail.com>
2019-07-16 11:50:53 -0700

    still fiddling with blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
      resolve: `gatsby-source-filesystem`,
      options: {
        path: `${__dirname}/blog`,
-       name: `markdown-pages`,
+       name: `blog`,
         },
       },
     `gatsby-transormer-remark`,
