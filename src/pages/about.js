a4550b6ae104ba375b2c0200829c38541b521b38
Sarah Aldous <sealdous@gmail.com>
2019-07-18 12:39:39 -0700

    added about and contact pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About This Site</h1>
+            <p>This site's entire purpose is to test out Netlify with Gatsby and see if it'll work for our purposes.</p>
+        </div>
+    )
+}
+
+export default AboutPage
